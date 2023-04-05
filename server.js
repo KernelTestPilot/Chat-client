c41ef2219d38247b001cfac264747543b0aa9420
KernelTestPilot <47748828+KernelTestPilot@users.noreply.github.com>
2023-04-05 10:32:06 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const database = require('./config/sqlConnection');
+const cors = require('cors')
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+
+
+  //testar så servern funkar, körs på 5000
+  res.send('Hello world');
+});
+
+
+
+
+
+require("./routes/ducks")(app);
+app.listen(5000, () => {
+  console.log(`Server is up and running on 5000 ...`);
+  
+});
