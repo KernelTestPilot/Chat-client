3c4d36a2bc175ac0ffc044bf6bce488514b4dc2e
KernelTestPilot <47748828+KernelTestPilot@users.noreply.github.com>
2023-04-05 10:32:23 +0200

    Add files via upload

---
+++ routes/ducks.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+
+    var router = require("express").Router();
+
+
+          
+    
+
+    // Create a new user
+  router.get("/channel", );
+
+
+
+    app.use('/ducks', router);
+  };
+
+  
